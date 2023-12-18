2b8ae2c66c7ac1f37c9365309a9c3f91d433d3f6
jasbner <jasbner@gmail.com>
2023-12-18 12:52:14 -0500

    load injury report data

--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -32,3 +32,45 @@ player_agg <- combined_players |> select(FD_Id = Id, FD_Nickname = Nickname, NBA
 player_agg
 
 usethis::use_data(player_agg, overwrite = TRUE)
+
+
+# injury report information -----------------------------------------------
+
+#attempt to read injury report
+library(pdftools)
+
+parse_injury_report = function(report_text){
+  df <- tibble(txt = read_lines(report_text))
+  df <- df |> filter(!txt == "") |> filter(!str_detect(txt,"Injury Report"))
+  df_colnames <- df[1,] |> str_split("  +") |> unlist()
+  dat <- df |> mutate(txt = gsub("  +","|",txt)) |> tidyr::separate_wider_delim(txt,"|", too_few = "align_end", names = df_colnames)
+  dat <- dat |> filter(!is.na(`Player Name`)) |> filter(!`Player Name` == "") |> select(-`Game Time`,-`Matchup`)
+  dat <- dat |> filter(grepl(",",`Player Name`))
+  dat <- dat |>  mutate_all(na_if,"")
+  dat <- dat |> fill(`Game Date`) |> fill(Team)
+  if("Category" %in% colnames(dat)){
+    dat <- dat |> unite("Reason",c(Category,Reason), sep = " - ")
+  }
+  if(!"Previous Status" %in% colnames(dat)){
+    dat <- dat |> mutate(`Previous Status` = NA_character_)
+  }
+  if("Previous Reason" %in% colnames(dat)){
+  dat <- dat |> select(-`Previous Reason`)
+  }
+  dat
+}
+
+files <- list.files("data-raw/injury_reports",pattern = "pdf$", full.names = TRUE)
+res_df <- parse_injury_report(pdf_text(files[1]))
+res_df$report_date <- files[1] |> str_extract("\\d+-\\d+-\\d+") |> as_date()
+files <- files[-1]
+for(f in files){
+  print(f)
+  res <- parse_injury_report(pdf_text(f))
+  res$report_date <- f |> str_extract("\\d+-\\d+-\\d+") |> as_date()
+  res_df <- rbind(res_df,res)
+
+}
+
+hist_injury_report <- res_df
+usethis::use_data(hist_injury_report, overwrite = TRUE)
