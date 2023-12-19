68a305bc6e6a03adbef54ab3cf94447c5cf50e11
jasbner <jasbner@gmail.com>
2023-12-19 07:33:26 -0500

    model fp per minute based on last 5 games

--- R/model_fp_per_min.R
+++ R/model_fp_per_min.R
@@ -9,7 +9,23 @@
 #' @examples
 model_fp_per_min <- function(game_stats){
   # get the last game played for everyone this year
-  fp_pred <- game_stats |> arrange(desc(GAME_DATE_EST)) |> group_by(PLAYER_ID) |> filter(row_number() == 1) |> ungroup() |> select(PLAYER_ID,fp_min)
+  # fp_pred <- game_stats |> arrange(desc(GAME_DATE_EST)) |> group_by(PLAYER_ID) |> filter(row_number() == 1) |> ungroup() |> select(PLAYER_ID,fp_min)
+  # this can be improved i couldnt figure out how to do this dynamically
+  mean_lag <- game_stats |>
+    group_by(TEAM_ID, PLAYER_ID) |>
+    arrange(desc(GAME_DATE_EST)) |>
+    filter(n()>5) |>
+    mutate(mean_lag_5 = rollmean(fp, k = 5, fill = "extend", align = "left")) |>
+    ungroup()
+
+  # get the set of values we want to predict
+  fp_pred <- mean_lag |>
+    group_by(PLAYER_ID) |>
+    arrange(desc(GAME_DATE_EST)) |>
+    filter(row_number() == 1) |>
+    filter(year(GAME_DATE_EST) == year(today())) |>
+    ungroup()
+
 
   fp_pred
 }
